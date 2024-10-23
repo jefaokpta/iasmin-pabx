5c196559601279f8056e6e0b15917fb3ba86a6fb
jefaokpta <jefaokpta@hotmail.com>
2024-10-23 14:33:36 -0300

    testando case onde demora o processamento de uma task

--- src/test/kotlin/br/com/vip/iasminpabx/record/RecordWorkerTest.kt
+++ src/test/kotlin/br/com/vip/iasminpabx/record/RecordWorkerTest.kt
@@ -2,10 +2,10 @@ package br.com.vip.iasminpabx.record
 
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Disabled
 import org.slf4j.LoggerFactory
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 /**
  * @author Jefferson Alves Reis (jefaokpta) < jefaokpta@hotmail.com >
@@ -19,17 +19,24 @@ class RecordWorkerTest {
     @Test
     fun runInOneVT() {
         val vt = Executors.newSingleThreadExecutor(Thread.ofVirtual().factory())
+//        val vt = Executors.newVirtualThreadPerTaskExecutor()
 
+        log.info("Executando tarefas")
         vt.submit {
             log.info("Executando em uma VT")
-            throw RuntimeException("Erro")
-            log.info("Executando em uma VT")
+//            throw RuntimeException("Erro")
+            log.info("Executando tarefa demorada")
+            TimeUnit.SECONDS.sleep(3)
         }
         vt.submit {
             log.info("Executando em outra VT")
             log.info("Executando em outra VT")
         }
+        vt.submit {
+            log.info("Executando em outra 3 VT")
+            log.info("Executando em outra 3 VT")
+        }
 
-
+        TimeUnit.SECONDS.sleep(5)
     }
 }
